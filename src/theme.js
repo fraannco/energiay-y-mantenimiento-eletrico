b0173f5c7b125cabfc8a6f3705869b536298319f
francosilva25 <e.franco250299@gmail.com>
2022-02-13 12:19:51 -0500

    Elimienando elementos iniciales

---
+++ src/theme.js
@@ -0,0 +1,15 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        type: 'light',
+        primary: {
+            main: '#d32f2f',
+        },
+        secondary: {
+            main: '#ff9100',
+        },
+    },
+});
+
+export default theme;
