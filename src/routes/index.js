20fbccef98cb4cfe4ef8b54960753cb36dfe589e
francosilva25 <e.franco250299@gmail.com>
2022-02-14 17:43:13 -0500

    Create HomePage

---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "../pages/HomePage/index"
+
+const EYM_ROUTES = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default EYM_ROUTES;
