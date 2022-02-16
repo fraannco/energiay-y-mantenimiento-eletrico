db3860a102900017664a8c39ed09a2b995bb3430
francosilva25 <e.franco250299@gmail.com>
2022-02-16 16:45:45 -0500

    Company: move data members to another file

---
+++ src/components/AboutsUs/AboutusData.js
@@ -0,0 +1,21 @@
+
+const AboutusData = () => {
+
+    const miembros = [
+        {
+            image: "./images/about-us/avatars/prueba.png",
+            name: "Samuel de Luque",
+            position: "Ceo of Sex",
+            desc: "Exercise addict and lifelong learner",
+            links: [
+                { type: "whatsapp", url: "https://wa.me/51929311362" },
+            ],
+        },
+    ];
+    return (
+        { miembros }
+    )
+}
+
+export default AboutusData
+
