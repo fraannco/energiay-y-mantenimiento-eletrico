27e51c0187cc0ec21f3c45767a460a00b7e6c4a9
francosilva25 <e.franco250299@gmail.com>
2022-02-16 14:49:18 -0500

    ObjetivoPrincipal component: Modify texts

--- src/components/AboutsUs/ObjetivoPrincipal.jsx
+++ src/components/AboutsUs/ObjetivoPrincipal.jsx
@@ -1,4 +1,4 @@
-import { Box, Container, Grid, Typography } from "@mui/material";
+import { Avatar, Box, Container, Grid, Typography } from "@mui/material";
 import CheckIcon from "@mui/icons-material/Check";
 import React from "react";
 
@@ -11,38 +11,41 @@ const objetivos = [
 
 const ObjetivoPrincipal = () => {
   return (
-    <Box bgcolor="#F9F9F9">
+    <Box bgcolor="#F9F9F9" py={5}>
       <Container>
-        <Grid container p={5}>
-          <Grid item md={7} xs={12} py={4}>
+        <Grid container p={4}>
+          <Grid item md={7}>
             <Typography variant="h2">Nuestro objetivo principal</Typography>
             <Typography variant="body1" my={1}>
-              Venimos trabajando con esmero con el único objetivo de poderles
-              brindar una excelente herramienta que les permita crecer y
-              aumentar las ventas y de la misma manera posesionar aún más los
-              negocios. Estos dos últimos años la pandemia nos ha golpeado muy
-              fuerte, pero ello no nos amilana para seguir adelante con lo que
-              ya nos hemos comprometido. Empezamos en el año 2010 publicando la
-              revista Energía & Negocios Perú, que luego de unos años paso a ser
-              lo que ahora la revista Energía & mantenimiento Eléctrico, medio
-              exclusivo de los eventos que realiza como es el Congreso
-              Internacional de Mantenimiento Eléctrico, Congreso de Trabajos con
-              Tensión TCT PERÚ, Seminario Internacional de Prospectiva,
-              Innovación y Tecnología- INNOVATEC PERÚ, entre otros.
+              Buscamos brindar una excelente herramienta que les permita crecer
+              y aumentar las ventas y posicionar mejor sus negocios. Ya somos
+              una comunidad numerosa conformada por profesionales y técnicos de
+              las áreas de mantenimiento eléctrico, seguridad eléctrica,
+              proyectos eléctricos, trabajos en líneas energizadas, etc.
             </Typography>
             <Typography variant="body1" my={1}>
-              Ya somos una comunidad numerosa que la conforma entre ellos
-              profesionales y técnicos de las áreas de mantenimiento eléctrico,
-              seguridad eléctrica, proyectos eléctricos, trabajos en líneas
-              energizadas, etc.
+              Te ofrecemos lo siguiente de manera honesta:
             </Typography>
             {objetivos.map((objetivo) => (
-              <Box key={objetivo} sx={{ display: "flex" }}>
-                <CheckIcon
-                  sx={{ maxWidth: "18px", marginRight: "0.5rem" }}
-                  color="primary"
-                />
-                <Typography variant="body2">{objetivo}</Typography>
+              <Box key={objetivo} sx={{ display: "flex" }} py={1}>
+                <Avatar
+                  sx={{
+                    maxWidth: "18px",
+                    maxHeight: "18px",
+                    backgroundColor: "#ffe6c2",
+                  }}
+                >
+                  <CheckIcon
+                    sx={{
+                      maxWidth: "14px",
+                      maxHeight: "14px",
+                      color: "#e67a00",
+                    }}
+                  />
+                </Avatar>
+                <Typography variant="body2" sx={{ paddingLeft: "0.5em" }}>
+                  {objetivo}
+                </Typography>
               </Box>
             ))}
           </Grid>
