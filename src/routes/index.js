999134a704ad2104bd86891e6b67a9e728b02da1
francosilva25 <e.franco250299@gmail.com>
2022-02-18 18:20:37 -0500

    Index: Add test component

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AboutUs from "../pages/About us";
+import PricingContent from "../components/prueba"
 
 const EYM_ROUTES = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/*" element={<AboutUs />} />
+        <Route path="/prueba" element={<PricingContent />} />
       </Routes>
     </BrowserRouter>
   );
