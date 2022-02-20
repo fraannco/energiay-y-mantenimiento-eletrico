523d474d7221f3fe7f5c45515c3159d36529bbd7
francosilva25 <e.franco250299@gmail.com>
2022-02-19 22:13:08 -0500

    Add about and home page in routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AboutUs from "../pages/About us";
 import PricingContent from "../components/prueba"
+import HomePage from "../pages/Homepage";
 
 const EYM_ROUTES = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/*" element={<AboutUs />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutUs />} />
         <Route path="/prueba" element={<PricingContent />} />
       </Routes>
     </BrowserRouter>
