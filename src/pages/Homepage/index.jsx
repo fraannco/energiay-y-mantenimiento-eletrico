1e1c4b575abd285f8efd35a8240c46b4a3ca4e3c
francosilva25 <e.franco250299@gmail.com>
2022-02-19 23:58:09 -0500

    Add new and footer component in homepage

--- src/pages/Homepage/index.jsx
+++ src/pages/Homepage/index.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import HeroSection from "../../components/HeroSection";
 import NavBar from "../../components/NavBar";
+import News from "../../components/News/News";
+import Footer from '../../components/Footer'
 
 const HomePage = () => {
   return (
     <>
       <NavBar />
       <HeroSection />
+      <News />
+      <Footer />
     </>
   );
 };
