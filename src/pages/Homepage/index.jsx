d71a49321237f0383da8b80a84535a8f882cf677
francosilva25 <e.franco250299@gmail.com>
2022-02-19 22:12:56 -0500

    Index for homepage

---
+++ src/pages/Homepage/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import HeroSection from "../../components/HeroSection";
+import NavBar from "../../components/NavBar";
+
+const HomePage = () => {
+  return (
+    <>
+      <NavBar />
+      <HeroSection />
+    </>
+  );
+};
+
+export default HomePage;
