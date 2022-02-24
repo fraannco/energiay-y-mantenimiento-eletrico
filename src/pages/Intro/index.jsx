6dd1492dfed4ac59d0049ead375a6274915a627e
francosilva25 <e.franco250299@gmail.com>
2022-02-24 16:26:16 -0500

    Create Introduction page

---
+++ src/pages/Intro/index.jsx
@@ -0,0 +1,13 @@
+import { Link, Typography } from "@mui/material";
+import React from "react";
+import InConstruction from "./InConstruction";
+
+const IntroductionPage = () => {
+  return (
+    <div>
+      <InConstruction />
+    </div>
+  );
+};
+
+export default IntroductionPage;
