cc349ccd9bff7c266a530ba9212ceba83bc3ee43
francosilva25 <e.franco250299@gmail.com>
2022-02-25 18:38:03 -0500

    Add introduction pages in all routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import AboutUs from "../pages/About us";
-import PricingContent from "../components/prueba"
-import HomePage from "../pages/Homepage";
+// import AboutUs from "../pages/About us";
+// import PricingContent from "../components/prueba"
+// import HomePage from "../pages/Homepage";
+import IntroductionPage from "../pages/Intro";
 
 const EYM_ROUTES = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutUs />} />
-        <Route path="/prueba" element={<PricingContent />} />
+        <Route path="/*" element={<IntroductionPage />} />
+        {/* <Route path="/about" element={<AboutUs />} />
+        <Route path="/prueba" element={<PricingContent />} /> */}
       </Routes>
     </BrowserRouter>
   );
