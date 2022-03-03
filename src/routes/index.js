ff23074f5b6573d8da97ad9a7bcce20cda2544c8
francosilva25 <e.franco250299@gmail.com>
2022-03-02 19:31:23 -0500

    Add Article Page to routes test

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AboutUs from "../pages/About us";
 import PricingContent from "../components/prueba"
 import HomePage from "../pages/Homepage";
+import Article from "../pages/Article/Article";
 
 const EYM_ROUTES = () => {
   return (
@@ -11,6 +12,7 @@ const EYM_ROUTES = () => {
         <Route path="/*" element={<HomePage />} />
         <Route path="/about" element={<AboutUs />} />
         <Route path="/prueba" element={<PricingContent />} />
+        <Route path="/article" element={<Article />} />
       </Routes>
     </BrowserRouter>
   );
