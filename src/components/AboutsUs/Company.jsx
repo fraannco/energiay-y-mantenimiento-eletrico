54b1083d72cd68b772f9876a220596521d001f02
francosilva25 <e.franco250299@gmail.com>
2022-03-04 23:14:57 -0500

    Add data in component

--- src/components/AboutsUs/Company.jsx
+++ src/components/AboutsUs/Company.jsx
@@ -11,9 +11,51 @@ import {
 import WhatsAppIcon from "@mui/icons-material/WhatsApp";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import React from "react";
-import AboutusData from "./AboutusData";
 
-const { miembros } = AboutusData();
+const miembros = [
+  {
+    image: "./images/about-us/avatars/miembro1.png",
+    name: "Juan José Silva Ramos",
+    position: "Generente general",
+    desc: "Adicto a los grandes retos",
+    links: [{ type: "whatsapp", url: "https://wa.me/51929311362" }],
+  },
+  {
+    image: "./images/about-us/avatars/miembro2.jpg",
+    name: "Marco Antonio Córdova",
+    position: "Generente de administración y finanzas",
+    desc: "Adicto a los grandes retos",
+    links: [{ type: "whatsapp", url: "https://wa.me/51929311362" }],
+  },
+  {
+    image: "./images/about-us/avatars/miembro3.jpg",
+    name: "Orlando Ardito Chávez",
+    position: "Comité de Seguridad Eléctrica",
+    desc: "Defensor de la ética y de la calidad",
+    links: [{ type: "whatsapp", url: "https://wa.me/51929311362" }],
+  },
+  {
+    image: "./images/about-us/avatars/miembro4.png",
+    name: "Edwin Dextre Jara",
+    position: "Comité de Prospectiva e Innovación",
+    desc: "Visionero y Forjador de nuevos  emprenderos",
+    links: [{ type: "whatsapp", url: "https://wa.me/51929311362" }],
+  },
+  {
+    image: "./images/about-us/avatars/miembro5.png",
+    name: "José A. Velásquez",
+    position: "Comité de Automatización Industrial",
+    desc: "Visionero de grandes proyectos  y mejora continua",
+    links: [{ type: "whatsapp", url: "https://wa.me/51929311362" }],
+  },
+  {
+    image: "./images/about-us/avatars/miembro6.png",
+    name: "Franklin Tamayo Ordaya",
+    position: "Comité de Trabajos con Tensión (TcT)",
+    desc: "Forjador de futuros forjadores en TcT",
+    links: [{ type: "whatsapp", url: "https://wa.me/51929311362" }],
+  },
+];
 
 const renderIcon = (type) => {
   switch (type) {
